0e7bbfed7d92c4d6427022d636264e1e6539ac92
Tony Chan <tonychan8201@gmail.com>
2023-12-17 17:30:43 +0800

    feat: add dist packages

---
+++ other-packages/react/dist/deepChat.d.ts
@@ -0,0 +1,6 @@
+import { DeepChat as DeepChatCore } from 'deep-chat';
+export declare const DeepChat: import("@lit-labs/react").ReactWebComponent<DeepChatCore, {
+    onactivate: string;
+    onchange: string;
+}>;
+//# sourceMappingURL=deepChat.d.ts.map
